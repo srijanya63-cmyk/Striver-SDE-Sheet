01090cc2c8d16039a440af22ae5fec997cce5b25
Srijanya <srijanya63@gmail.com>
2026-08-18 21:24:39 +0530

    Time: 2 ms (89.55%) | Memory: 44.1 MB (72.68%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,67 +1,58 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int[][] time = new int[n][m];
 
-        Queue<int[]> queue = new LinkedList<>();
-        int fresh = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                time[i][j] = Integer.MAX_VALUE;
+            }
+        }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+                    dfs(grid, time, i, j, 0);
                 }
             }
         }
 
-        if (fresh == 0) {
-            return 0;
-        }
+        int ans = 0;
 
-        int minutes = 0;
-
-        int[][] dir = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
-        };
-
-        while (!queue.isEmpty() && fresh > 0) {
-
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-
-                int[] curr = queue.poll();
-
-                int row = curr[0];
-                int col = curr[1];
-
-                for (int[] d : dir) {
-
-                    int newRow = row + d[0];
-                    int newCol = col + d[1];
-
-                    if (newRow >= 0 && newRow < rows &&
-                        newCol >= 0 && newCol < cols &&
-                        grid[newRow][newCol] == 1) {
-
-                        grid[newRow][newCol] = 2;
-                        fresh--;
-
-                        queue.offer(new int[]{newRow, newCol});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    if (time[i][j] == Integer.MAX_VALUE) {
+                        return -1;
                     }
+                    ans = Math.max(ans, time[i][j]);
                 }
             }
-
-            minutes++;
         }
 
-        return fresh == 0 ? minutes : -1;
+        return ans;
+    }
+
+    private void dfs(int[][] grid, int[][] time, int r, int c, int currentTime) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
+            return;
+        }
+
+        if (grid[r][c] == 0) {
+            return;
+        }
+
+        if (currentTime >= time[r][c]) {
+            return;
+        }
+
+        time[r][c] = currentTime;
+
+        dfs(grid, time, r + 1, c, currentTime + 1);
+        dfs(grid, time, r - 1, c, currentTime + 1);
+        dfs(grid, time, r, c + 1, currentTime + 1);
+        dfs(grid, time, r, c - 1, currentTime + 1);
     }
 }
